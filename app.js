86eeb49d08a86f3a80cacf220d5392a5481e0ca4
Billy Sheppard <billy.sheppard@eu.clara.net>
2019-03-27 11:04:56 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const axios = require("axios");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+const domainRegistrar = {
+  getDomainsByRegistrar: async function(registrar) {
+    const HTML = await domainRegistrar.getHTML(registrar);
+    const domains = await domainRegistrar.scrapeHTML(HTML);
+    return Promise.resolve(domains);
+  },
+  getHTML: async function(registrar) {
+    if (!registrar) throw "ERROR: No registrar provided.";
+    const instance = axios.create({
+      baseURL: "https://viewdns.info/reversewhois/",
+      timeout: 5000
+    });
+    const response = await instance.get(`?q=${encodeURI(registrar)}`);
+    return response.data;
+  },
+
+  scrapeHTML: function(html) {
+    const dom = new JSDOM(html);
+    const scrapedData = Array.from(
+      dom.window.document.querySelectorAll(
+        "#null > tbody > tr:nth-child(3) > td > font > table > tbody tr"
+      )
+    ).map(e => {
+      return {
+        domain: e.querySelectorAll("td")[0].innerHTML,
+        creation: e.querySelectorAll("td")[1].innerHTML,
+        registrar: e.querySelectorAll("td")[2].innerHTML
+      };
+    });
+    return scrapedData;
+  }
+};
+
+module.exports = domainRegistrar;
