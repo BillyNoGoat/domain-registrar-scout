1e8dc8c169fb1e5a29bb268e67926c434edba8f3
BillyGoat <goatsniffing@gmail.com>
2019-04-23 15:27:12 +0100

    Added simple whois lookup for domain

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const axios = require("axios");
 const jsdom = require("jsdom");
+const whois = require("whois-json");
+var _ = require("lodash");
 const { JSDOM } = jsdom;
 
 const domainRegistrant = {
@@ -9,6 +11,17 @@ const domainRegistrant = {
     // Pass to scraper to return object of data
     const domains = await domainRegistrant.scrapeHTML(HTML);
     return Promise.resolve(domains);
+  },
+  // whois lookup on the domain, returns registrant information
+  getRegistrantByDomain: async function(domain) {
+    const registrantFull = await whois(domain);
+    const registrant = _.pick(registrantFull, [
+      "registrantName",
+      "registrantOrganization",
+      "registrantPostalCode",
+      "registrantEmail"
+    ]);
+    return Promise.resolve(JSON.stringify(registrant, null, 2));
   },
   // Pull HTML based on registrant
   getHTML: async function(registrant) {
@@ -41,3 +54,18 @@ const domainRegistrant = {
 };
 
 module.exports = domainRegistrant;
+
+/*
+https://github.com/whisp1830/domains_mine/blob/fe76a78d74488b9c7a675d018667bea6c2af1560/whois_reverse/anadomain.py
+
+'REDACTED FOR PRIVACY',
+                        '******** ******** (see Notes section below on how to view unmasked data)',
+                        'Data Protected Data Protected',
+                        'Domain Admin',
+                        'SLD Admin',
+                        'Nexperian Holding Limited',
+                        'Registration Private',
+                        'Whois Agent',
+                        'WhoisGuard Protected',
+                        'yinsi baohu yi kai qi(Hidden by Whois Privacy Protection Service)','']:
+*/
